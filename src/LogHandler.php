14932c41dfbc7c51bfef1b2f51cb3e60c419bdc2
Alvin Chen <alvin.chen@alvin.local>
2018-05-10 15:36:28 -0700

    add handleException static method

--- src/LogHandler.php
+++ src/LogHandler.php
@@ -12,14 +12,14 @@ use Bramus\Monolog\Formatter\ColoredLineFormatter;
 
 class LogHandler {
 
-    public static function getLogger() {
+    public static function getLogger($log_file_path) {
 
         // initialized logger
         $logger = new Logger(getenv('APP_ENV'));
         ErrorHandler::register($logger);
 
         // set up file logging
-        $file_handler = new StreamHandler($_SERVER['DOCUMENT_ROOT']. "/logs/" . Date('Y/M') . ".log", Logger::DEBUG);
+        $file_handler = new StreamHandler($log_file_path, Logger::DEBUG);
         $file_formatter = new LineFormatter(null, null, false, true);
         $file_handler->setFormatter($file_formatter);
         $logger->pushHandler($file_handler);
@@ -41,6 +41,17 @@ class LogHandler {
 
         return $logger;
 
+    }
+
+    public static function handleException($logger, $e, $sfCustomerId) {
+        if (isset($logger) && $logger instanceof Monolog\Logger) {
+            $logger->error("Request: $sfCustomerId", [
+                'ip' => isset($_SERVER['X_FORWARDED_FOR']) ? $_SERVER['X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR'],
+                'http_referer' => isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : "",
+                'error' => $e->getMessage(),
+            ]);
+            $logger->debug('ENVIRONMENT', array_merge($_SERVER, $_GET));
+        }
     }
 
 }
